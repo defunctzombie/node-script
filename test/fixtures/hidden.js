cb909c6974c1729b19ed239fd6cce50fd65035fa
Roman Shtylman <shtylman@gmail.com>
2012-04-11 16:49:31 -0400

    test: hidden require in function

--- test/fixtures/hidden.js
+++ test/fixtures/hidden.js
@@ -1,4 +1,8 @@
-/// even requires in if statements should be found
+/// even requires in if statements and functions should be found
+
+function func() {
+    require('./single');
+}
 
 if (hi) {
     require('./noop');
--- test/golden/hidden.js
+++ test/golden/hidden.js
@@ -1,10 +1,20 @@
 require.register('__entry__', function(module, exports, require, __filename, __dirname) {
-/// even requires in if statements should be found
+/// even requires in if statements and functions should be found
+
+function func() {
+    require('./single');
+}
 
 if (hi) {
     require('./noop');
 }
 
+});
+
+require.register('./single', function(module, exports, require, __filename, __dirname) {
+/// single require
+
+var noop = require('./noop');
 });
 
 require.register('./noop', function(module, exports, require, __filename, __dirname) {
